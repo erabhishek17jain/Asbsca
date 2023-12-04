44b42674f989b4a921c362d3f2d230306f476c09
Abhishek Jain <erabhishek17jain@gmail.com>
2023-12-04 14:06:19 +0530

    config

--- server/config.ts
+++ server/config.ts
@@ -1,5 +1,16 @@
 const CONFIG = {
+    JWT_SECRET: 'asbsca-app',
+    EMAIL: 'mateo.jerde@ethereal.email',
+    PASSWORD: 'C2gF9hDN7CQe1H5BUZ',
     ATLAS_URI: 'mongodb+srv://3asoftwares:karLM1Doo1PvrdBF@abasca.sxdkgor.mongodb.net/abasca',
+    CLIENT_ID: '857985018804-abl6p7nrbrn3cmq6hsijilr3ajppetpa.apps.googleusercontent.com',
+    CLIENT_SECRET: '',
+    email: {
+        user: '3ASoftwares@gmail.com',
+        pass: 'zularurktxjctzqb',
+    },
+    ADMIN_ROLE_ID: '654e7db499e3242d43db55ef',
+    CLIENT_URL: 'https://asbsca.vercel.app/',
 };
 
 export default CONFIG;
